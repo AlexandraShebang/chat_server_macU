af50a4dd7810b692f02b65fc258e8be95f2a9b4e
Sri Sharada Kalyanaraman <srisharada.kalyanaraman@students.mq.edu.au>
2026-08-30 23:09:51 +1000

    creating weeds

--- project/COMP2000-Simulation/src/Weeds.java
+++ project/COMP2000-Simulation/src/Weeds.java
@@ -4,6 +4,23 @@ import java.awt.*;
 public class Weeds extends Plant{
     int size;
 
-    
+    Weeds(Point position, int size){
+        super(position, size);
+        this.size = size;
+        this.position = position;
+        this.setBackground(Color.darkGray);
+    }
+
+    @Override
+
+    public void spread(){
+        Radius radius = new Radius(position, speadRadius);
+
+        for(int i = 0; o < speadNum; i++){
+            Point newPoint = radius.getRandomPoint();
+            Weeds child = new Weeds(newPoint, size);
+        }
+    }
+
     
 }
