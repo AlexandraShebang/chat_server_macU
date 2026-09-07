e1fff48f8e19b584290408ce911a1a8ca60d8d1d
AlexandraShebang <alexandrajloh@gmail.com>
2026-09-08 09:00:14 +1000

    added a new time of day logic loop and helper function

--- project/COMP2000-Simulation/src/Controller.java
+++ project/COMP2000-Simulation/src/Controller.java
@@ -9,4 +9,23 @@ public class Controller {
 
         }
     };
+    public int timer() { //thought this could be useful to keep track of time across the simulation
+        int t = 24; //seperate value so it is easier when adjusting the rate at which time passes 
+        int hours = 0;
+        for (int i = 0; i < t; i++) {
+            hours++;
+        }
+        return hours;
+    }
+
+    public boolean daytime() { //returns true if its day, false if not
+        int gethours = timer();
+        int dawn = 6; //adjust this if you need to alter the time at which the sun rises
+        int dusk = 18; //adjust this if you need to alter the time at which the sun sets
+        if (gethours <= dawn && gethours > dusk) {
+        } else {
+            return true;
+        }
+        return false;
+    }
 }
